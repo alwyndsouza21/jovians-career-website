7a5acec927dd1bf5033b3316af0fe313cdd43bd3
alwyndsouza21 <96863495+alwyndsouza21@users.noreply.github.com>
2024-04-11 14:51:10 +0000

    used bootstap to add styling and listed jobs

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask,render_template
 app=Flask(__name__)
+jobs=[
+  {
+    "id":1,
+    "title":"Data Analyst",
+    "location":"Bengaluru,India",
+    "Salary":"Rs. 10,00,000"
+  },
+  {
+    "id":2,
+      "title":"Job Analyst",
+      "location":"Remote",
+      "Salary":"Rs. 15,00,000"
+    },
+{
+  "id":3,
+    "title":"Data Engineer",
+    "location":"Udupi,India",
+    "Salary":"Rs. 10,00,000"
+  }
 
+]
 @app.route("/")
 def hello():
- return render_template("home.html")
+ return render_template("home.html",jobs=jobs,company_name="JOVIAN")
 if __name__=="__main__":
   app.run(host="0.0.0.0",debug=True)
   
